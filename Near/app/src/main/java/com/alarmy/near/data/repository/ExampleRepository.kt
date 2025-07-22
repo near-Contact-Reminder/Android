d04d285ff075eae56e1f3e28e00bffaec813fd63
kwakjoohyeong <rhkrwngud445@gmail.com>
2025-07-23 00:35:18 +0900

    feat: repository example 추가

--- Near/app/src/main/java/com/alarmy/near/data/repository/ExampleRepository.kt
+++ Near/app/src/main/java/com/alarmy/near/data/repository/ExampleRepository.kt
@@ -1,5 +1,9 @@
 package com.alarmy.near.data.repository
 
+import kotlinx.coroutines.flow.Flow
+
 interface ExampleRepository {
     fun getExampleData(): String
+
+    fun getData(): Flow<String>
 }
--- Near/app/src/main/java/com/alarmy/near/data/repository/ExampleRepositoryImpl.kt
+++ Near/app/src/main/java/com/alarmy/near/data/repository/ExampleRepositoryImpl.kt
@@ -1,9 +1,17 @@
 package com.alarmy.near.data.repository
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class ExampleRepositoryImpl
     @Inject
     constructor() : ExampleRepository {
         override fun getExampleData(): String = "Hello from Repository"
+
+        override fun getData(): Flow<String> =
+            flow {
+                // val result = exampleService.fetchExample()
+                // emit(result.data)
+            }
     }
