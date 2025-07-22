2a47535688f7326e2b218b805a7e2c7d5e19c498
kwakjoohyeong <rhkrwngud445@gmail.com>
2025-07-22 21:44:44 +0900

    feat: NearApp 분리

---
+++ Near/app/src/main/java/com/alarmy/near/feature/main/NearApp.kt
@@ -0,0 +1,22 @@
+package com.alarmy.near.feature.main
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+internal fun NearApp(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+) {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        NearNavHost(
+            modifier = Modifier.padding(innerPadding),
+            navController = navController,
+        )
+    }
+}
