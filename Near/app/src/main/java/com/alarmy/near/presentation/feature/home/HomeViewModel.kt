d4d2e8559bd81f8732b5c677fff2623b646e5e44
kwakjoohyeong <rhkrwngud445@gmail.com>
2025-07-23 23:13:43 +0900

    feat: viewModel 단일 초기화 예시 추가

--- Near/app/src/main/java/com/alarmy/near/presentation/feature/home/HomeViewModel.kt
+++ Near/app/src/main/java/com/alarmy/near/presentation/feature/home/HomeViewModel.kt
@@ -6,8 +6,11 @@ import com.alarmy.near.data.repository.ExampleRepository
 import com.alarmy.near.presentation.feature.home.model.HomeUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -17,8 +20,21 @@ class HomeViewModel
     constructor(
         private val exampleRepository: ExampleRepository,
     ) : ViewModel() {
+        // Example: 여러번 초기화되는 StateFlow
         private val _uiStateFlow = MutableStateFlow(HomeUiState.Loading)
         val uiStateFlow = _uiStateFlow.asStateFlow()
+
+        // Example: 한 번만 초기화되는 StateFlow
+        private val exampleStateFlow =
+            exampleRepository
+                .getData()
+                .map {
+                    // Mapping to UIState
+                }.stateIn(
+                    scope = viewModelScope,
+                    started = SharingStarted.WhileSubscribed(5_000),
+                    initialValue = HomeUiState.Loading,
+                )
 
         fun fetchContacts() {
             viewModelScope.launch {
