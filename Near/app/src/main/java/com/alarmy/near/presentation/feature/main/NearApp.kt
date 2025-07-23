d007f8b8b6640e6e8ff28ddc98966ede59e872b0
kwakjoohyeong <rhkrwngud445@gmail.com>
2025-07-23 23:29:13 +0900

    feat: snackbar 호출로직 구성

--- Near/app/src/main/java/com/alarmy/near/presentation/feature/main/NearApp.kt
+++ Near/app/src/main/java/com/alarmy/near/presentation/feature/main/NearApp.kt
@@ -1,22 +1,57 @@
 package com.alarmy.near.presentation.feature.main
 
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.exclude
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.launch
 
 @Composable
 internal fun NearApp(
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController(),
 ) {
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+    val snackBarState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        snackbarHost = {
+            SnackbarHost(
+                hostState = snackBarState,
+                modifier =
+                    Modifier.windowInsetsPadding(
+                        WindowInsets.safeDrawing.exclude(
+                            WindowInsets.ime,
+                        ),
+                    ),
+            )
+        },
+    ) { innerPadding ->
         NearNavHost(
             modifier = Modifier.padding(innerPadding),
             navController = navController,
+            onShowSnackbar = {
+                scope.launch {
+                    snackBarState.showSnackbar(
+                        message = it?.message ?: return@launch,
+                        duration = SnackbarDuration.Short,
+                    )
+                }
+            },
         )
     }
 }
