e0d3c8b132fd6790c2ab931f011b75267e733dd3
kwakjoohyeong <rhkrwngud445@gmail.com>
2025-07-30 01:31:17 +0900

    feat: Radio Button 추가

--- Near/app/src/main/java/com/alarmy/near/presentation/ui/component/radiobutton/NearSmallRadioButton.kt
+++ Near/app/src/main/java/com/alarmy/near/presentation/ui/component/radiobutton/NearSmallRadioButton.kt
@@ -1,4 +1,4 @@
-package com.alarmy.near.presentation.ui.component.checkbox
+package com.alarmy.near.presentation.ui.component.radiobutton
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
@@ -14,21 +14,21 @@ import androidx.compose.ui.unit.dp
 import com.alarmy.near.R
 
 @Composable
-fun NearCheckbox(
+fun NearSmallRadioButton(
     modifier: Modifier = Modifier,
-    checked: Boolean,
-    onCheckedChange: (Boolean) -> Unit,
+    selected: Boolean,
+    onClick: (Boolean) -> Unit,
 ) {
     Image(
         modifier =
             modifier.clickable(
-                onClick = { onCheckedChange(!checked) },
+                onClick = { onClick(!selected) },
             ),
         painter =
-            if (checked) {
-                painterResource(R.drawable.btn_checkbox_h2_on)
+            if (selected) {
+                painterResource(R.drawable.btn_radio_h2_on)
             } else {
-                painterResource(R.drawable.btn_checkbox_h2_off)
+                painterResource(R.drawable.btn_radio_h2_off)
             },
         contentDescription = null,
     )
@@ -36,17 +36,17 @@ fun NearCheckbox(
 
 @Preview(widthDp = 360, heightDp = 70, showBackground = true)
 @Composable
-fun NearCheckboxPreview() {
+fun NearSmallRadioButtonPreview() {
     Surface {
         Row {
-            NearCheckbox(
-                checked = true,
-                onCheckedChange = {},
+            NearSmallRadioButton(
+                selected = true,
+                onClick = {},
             )
             Spacer(modifier = Modifier.width(50.dp))
-            NearCheckbox(
-                checked = false,
-                onCheckedChange = {},
+            NearSmallRadioButton(
+                selected = false,
+                onClick = {},
             )
         }
     }
