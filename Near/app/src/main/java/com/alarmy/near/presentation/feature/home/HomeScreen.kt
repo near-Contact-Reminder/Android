6a34e47cae68ebfcba6ae16e30ebc667dd507b1f
kwakjoohyeong <rhkrwngud445@gmail.com>
2025-08-01 18:30:16 +0900

    feat: Home 배경 적용

--- Near/app/src/main/java/com/alarmy/near/presentation/feature/home/HomeScreen.kt
+++ Near/app/src/main/java/com/alarmy/near/presentation/feature/home/HomeScreen.kt
@@ -1,15 +1,23 @@
 package com.alarmy.near.presentation.feature.home
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.alarmy.near.R
 import com.alarmy.near.presentation.feature.home.model.HomeUiState
 import com.alarmy.near.presentation.ui.theme.NearTheme
 
@@ -26,6 +34,7 @@ internal fun HomeRoute(
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun HomeScreen(
     modifier: Modifier = Modifier,
@@ -33,7 +42,21 @@ internal fun HomeScreen(
     onContactClick: (Long) -> Unit = { _ -> },
     onRemoveContact: (Long) -> Unit = { _ -> },
 ) {
-    Column(modifier = Modifier.fillMaxSize().background(Color.White)) {
+    val density = LocalDensity.current
+    val statusBarHeightDp = with(density) { WindowInsets.statusBars.getTop(density).toDp() }
+
+    Column(
+        modifier =
+            Modifier
+                .paint(
+                    painter =
+                        painterResource(
+                            R.drawable.img_bg,
+                        ),
+                    contentScale = ContentScale.FillBounds,
+                ).fillMaxSize(),
+    ) {
+        Spacer(modifier = Modifier.height(statusBarHeightDp))
         Text("홈 화면")
     }
 }
