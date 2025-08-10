1c770b58f66f2cad0d8d15c738f560eaf9d77abc
kwakjoohyeong <rhkrwngud445@gmail.com>
2025-08-10 17:16:55 +0900

    feat: addContactImage 추가

---
+++ Near/app/src/main/java/com/alarmy/near/presentation/feature/home/component/AddContactImage.kt
@@ -0,0 +1,27 @@
+package com.alarmy.near.presentation.feature.home.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.alarmy.near.R
+
+@Composable
+fun AddContactImage(modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier,
+        painter = painterResource(R.drawable.ic_64_adduser),
+        contentDescription =
+            stringResource(
+                R.string.home_add_contact,
+            ),
+    )
+}
+
+@Preview(widthDp = 48, heightDp = 48)
+@Composable
+fun AddContactImagePreview() {
+    AddContactImage()
+}
