1a0f6c6b6600b9692a8c0fb586d3049aa0312d57
kwakjoohyeong <rhkrwngud445@gmail.com>
2025-08-17 18:20:06 +0900

    feat: ReminderInterval 모델 추가

---
+++ Near/app/src/main/java/com/alarmy/near/model/ReminderInterval.kt
@@ -0,0 +1,14 @@
+package com.alarmy.near.model
+
+import androidx.annotation.StringRes
+import com.alarmy.near.R
+
+enum class ReminderInterval(
+    @param:StringRes val value: Int,
+) {
+    DAILY(R.string.reminder_interval_daily), // 매일
+    WEEKLY(R.string.reminder_interval_weekly), // 매주
+    BIWEEKLY(R.string.reminder_interval_biweekly), // 2주
+    MONTHLY(R.string.reminder_interval_monthly), // 매달
+    SEMIANNUAL(R.string.reminder_interval_semiannual), // 6개월
+}
