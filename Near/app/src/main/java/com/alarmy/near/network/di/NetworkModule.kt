78f229d0802d35e30793f9aa715c9333639e1feb
kwakjoohyeong <rhkrwngud445@gmail.com>
2025-08-19 20:34:12 +0900

    feat: Retrofit & Okhttp DI 구성

---
+++ Near/app/src/main/java/com/alarmy/near/network/di/NetworkModule.kt
@@ -0,0 +1,54 @@
+package com.alarmy.near.network.di
+
+import com.alarmy.near.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    private val json =
+        Json {
+            encodeDefaults = true
+            ignoreUnknownKeys = true
+            prettyPrint = true
+            isLenient = true
+        }
+
+    private val jsonConverterFactory = json.asConverterFactory("application/json".toMediaType())
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient =
+        OkHttpClient.Builder().addInterceptor(loggingInterceptor).build()
+
+    @Provides
+    @Singleton
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().let {
+            if (BuildConfig.DEBUG) {
+                it.setLevel(HttpLoggingInterceptor.Level.BODY)
+            } else {
+                it.setLevel(HttpLoggingInterceptor.Level.NONE)
+            }
+        }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
+        Retrofit
+            .Builder()
+            .baseUrl(BuildConfig.NEAR_URL)
+            .client(okHttpClient)
+            .addConverterFactory(jsonConverterFactory)
+            .build()
+}
