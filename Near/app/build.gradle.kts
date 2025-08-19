9b3e75952eff6f865b19da97859f749aa659abe5
kwakjoohyeong <rhkrwngud445@gmail.com>
2025-08-19 20:20:53 +0900

    feat: 네트워크 라이브러리 및 빌드 구성

--- Near/app/build.gradle.kts
+++ Near/app/build.gradle.kts
@@ -1,3 +1,9 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+import java.util.Properties
+
+val localProperties =
+    Properties().apply { rootProject.file("local.properties").inputStream().use { load(it) } }
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -28,6 +34,10 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
             )
+            buildConfigField("String", "NEAR_URL", getProperty("NEAR_PROD_URL"))
+        }
+        debug {
+            buildConfigField("String", "NEAR_URL", getProperty("NEAR_DEV_URL"))
         }
     }
     compileOptions {
@@ -38,6 +48,7 @@ android {
         jvmTarget = "11"
     }
     buildFeatures {
+        buildConfig = true
         compose = true
     }
 }
@@ -63,9 +74,10 @@ dependencies {
     implementation(libs.hilt.android)
     kapt(libs.hilt.android.compiler)
     implementation(libs.hilt.navigation.compose)
-    // Retrofit
+    // Retrofit & OkHttp
     implementation(libs.retrofit)
-    implementation(libs.retrofit.converter.gson)
+    implementation(libs.retrofit.kotlin.serialization.converter)
+    implementation(libs.logging.interceptor)
     // Glide
     implementation(libs.glide)
     kapt(libs.glide.compiler)
@@ -79,3 +91,5 @@ dependencies {
     // Serialization
     implementation(libs.kotlin.serialization.json)
 }
+
+fun getProperty(propertyKey: String): String = gradleLocalProperties(rootDir, providers).getProperty(propertyKey)
