7933609f279d42ee1513ab1b52d385cf6d1a22fc
kwakjoohyeong <rhkrwngud445@gmail.com>
2025-08-27 00:33:34 +0900

    fix: 챙김 기록 Entity 직렬화 적용

--- Near/app/src/main/java/com/alarmy/near/network/response/FriendRecordEntity.kt
+++ Near/app/src/main/java/com/alarmy/near/network/response/FriendRecordEntity.kt
@@ -1,5 +1,8 @@
 package com.alarmy.near.network.response
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class FriendRecordEntity(
     val isChecked: Boolean,
     val createdAt: String,
