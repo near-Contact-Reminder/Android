cd854b6b424d9e2ef993782448a327a2cec96e9c
kwakjoohyeong <rhkrwngud445@gmail.com>
2025-09-10 20:38:07 +0900

    feat: 연락처 기념일, 라벨 등 추가정보 적용

--- Near/app/src/main/java/com/alarmy/near/local/entity/ContactEntity.kt
+++ Near/app/src/main/java/com/alarmy/near/local/entity/ContactEntity.kt
@@ -7,4 +7,11 @@ data class ContactEntity(
     val photoUri: String?, // 사진
     val birthDay: String?, // 생일
     val memo: String?, // 메모
+    val groups: List<String>, // 그룹 (ex. 가족, 친구, 회사)
+    val importantDates: List<ImportantDate>,
+)
+
+data class ImportantDate(
+    val label: String,
+    val date: String,
 )
