4392caf53edbedea157af08477b49081f2b57f6a
kwakjoohyeong <rhkrwngud445@gmail.com>
2025-09-10 20:38:20 +0900

    feat: 연락처 화면 파일 추가

---
+++ Near/app/src/main/java/com/alarmy/near/presentation/feature/contact/ContactScreen.kt
@@ -0,0 +1,12 @@
+package com.alarmy.near.presentation.feature.contact
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ContactRoute() {
+}
+
+@Composable
+fun ContactScreen(modifier: Modifier = Modifier) {
+}
