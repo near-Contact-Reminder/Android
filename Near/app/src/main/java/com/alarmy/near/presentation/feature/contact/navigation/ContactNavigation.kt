6e24a7714775bf1fb4a56f8786e5643dad1d2e4b
kwakjoohyeong <rhkrwngud445@gmail.com>
2025-09-14 15:10:20 +0900

    feat: 연락처 상단바 추가

---
+++ Near/app/src/main/java/com/alarmy/near/presentation/feature/contact/navigation/ContactNavigation.kt
@@ -0,0 +1,26 @@
+package com.alarmy.near.presentation.feature.contact.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.alarmy.near.presentation.feature.contact.ContactRoute
+import kotlinx.serialization.Serializable
+
+@Serializable
+object RouteContact
+
+/*
+* 추후 홈으로 화면 이동이 필요할 때 이 함수를 사용합니다.
+* */
+fun NavController.navigateToContact(navOptions: NavOptions) {
+    navigate(RouteContact, navOptions)
+}
+
+fun NavGraphBuilder.contactNavGraph(onShowErrorSnackBar: (throwable: Throwable?) -> Unit) {
+    composable<RouteContact> { backStackEntry ->
+        ContactRoute(
+            onShowErrorSnackBar = onShowErrorSnackBar,
+        )
+    }
+}
