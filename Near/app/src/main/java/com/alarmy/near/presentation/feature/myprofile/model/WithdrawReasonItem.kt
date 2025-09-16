fddabb707a4cb85912fa79569df4ba7bc2a3a748
stopstone <wltjr6432@naver.com>
2025-09-17 02:32:09 +0900

    remove: 회원 탈퇴 사유 모델 제거

--- Near/app/src/main/java/com/alarmy/near/presentation/feature/myprofile/model/WithdrawReasonItem.kt
+++
@@ -1,9 +0,0 @@
-package com.alarmy.near.presentation.feature.myprofile.model
-
-/**
- * 탈퇴 사유 데이터 클래스
- */
-data class WithdrawReasonItem(
-    val reason: WithdrawReason,
-    val isSelected: Boolean = false
-)
