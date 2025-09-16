634872a31f0825da7d6862432071b2e26f9e1f32
stopstone <wltjr6432@naver.com>
2025-09-17 01:25:53 +0900

    chore: 미사용 import 제거

--- Near/app/src/main/java/com/alarmy/near/presentation/feature/myprofile/components/NearServiceInfoRow.kt
+++ Near/app/src/main/java/com/alarmy/near/presentation/feature/myprofile/components/NearServiceInfoRow.kt
@@ -1,6 +1,5 @@
 package com.alarmy.near.presentation.feature.myprofile.components
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
