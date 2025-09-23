0da3c58c9f30f02e619f36b8bd3e9c60246d4e5c
kwakjoohyeong <rhkrwngud445@gmail.com>
2025-09-24 00:59:58 +0900

    feat: 연락처 상단바 추가
    # Conflicts:
    #	Near/app/src/main/res/values/strings.xml

--- Near/app/src/main/java/com/alarmy/near/presentation/feature/contact/ContactScreen.kt
+++ Near/app/src/main/java/com/alarmy/near/presentation/feature/contact/ContactScreen.kt
@@ -1,12 +1,65 @@
 package com.alarmy.near.presentation.feature.contact
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.alarmy.near.R
+import com.alarmy.near.presentation.ui.component.NearFrame
+import com.alarmy.near.presentation.ui.theme.NearTheme
 
 @Composable
-fun ContactRoute() {
+fun ContactRoute(onShowErrorSnackBar: (throwable: Throwable?) -> Unit) {
+    ContactScreen()
 }
 
 @Composable
-fun ContactScreen(modifier: Modifier = Modifier) {
+fun ContactScreen(
+    modifier: Modifier = Modifier,
+    onBackClick: () -> Unit = {},
+) {
+    NearFrame(modifier = modifier) {
+        Row(
+            modifier =
+                Modifier.fillMaxWidth().padding(
+                    start = 24.dp,
+                    end = 20.dp,
+                    top = 8.dp,
+                    bottom = 8.dp,
+                ),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                stringResource(R.string.contact_title_text),
+                style = NearTheme.typography.B1_16_BOLD,
+                color = NearTheme.colors.BLACK_1A1A1A,
+            )
+            Image(
+                painter = painterResource(R.drawable.ic_close_32_black),
+                contentDescription = stringResource(R.string.contact_close_screen),
+            )
+        }
+        Spacer(modifier = Modifier.height(12.dp))
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ContactScreenPreview() {
+    NearTheme {
+        ContactScreen { }
+    }
 }
