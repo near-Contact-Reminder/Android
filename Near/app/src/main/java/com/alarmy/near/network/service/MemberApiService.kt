4522d0f36b057b92d2eb98a2f4135b0ea3ea446c
stopstone <wltjr6432@naver.com>
2025-09-23 14:30:40 +0900

    refactor: API 응답 타입 수정
    - `MemberApiService`의 `getMyInfo` 함수의 반환 타입을 `Response<MemberInfo>`에서 `MemberInfo`로 변경했습니다.
    - `withdraw` 함수의 반환 타입을 `Response<Unit>`에서 `Unit`으로 변경했습니다.

--- Near/app/src/main/java/com/alarmy/near/network/service/MemberApiService.kt
+++ Near/app/src/main/java/com/alarmy/near/network/service/MemberApiService.kt
@@ -2,11 +2,9 @@ package com.alarmy.near.network.service
 
 import com.alarmy.near.model.member.MemberInfo
 import com.alarmy.near.model.member.WithdrawRequest
-import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.HTTP
-import retrofit2.http.Header
 
 /**
  * 회원 관련 API 서비스
@@ -14,11 +12,11 @@ import retrofit2.http.Header
 interface MemberApiService {
     // 현재 로그인한 회원의 정보를 조회
     @GET("member/me")
-    suspend fun getMyInfo(): Response<MemberInfo>
+    suspend fun getMyInfo(): MemberInfo
 
     // 회원 탈퇴
     @HTTP(method = "DELETE", path = "member/withdraw", hasBody = true)
     suspend fun withdraw(
         @Body request: WithdrawRequest,
-    ): Response<Unit>
+    )
 }
