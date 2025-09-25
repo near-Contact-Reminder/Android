ea3f2735bbb3ad22bdf488df026a5ff1a0e718e9
StopStone <wltjr6432@naver.com>
2025-09-26 01:25:45 +0900

    refactor: 이용약관 동의 항목 UI 개선
    - 우측 끝에 > 이미지를 추가합니다

--- Near/app/src/main/java/com/alarmy/near/presentation/feature/login/components/TermsAgreementItem.kt
+++ Near/app/src/main/java/com/alarmy/near/presentation/feature/login/components/TermsAgreementItem.kt
@@ -1,8 +1,9 @@
 package com.alarmy.near.presentation.feature.login.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.HorizontalDivider
@@ -10,7 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.alarmy.near.R
 import com.alarmy.near.presentation.ui.component.checkbox.NearCheckbox
 import com.alarmy.near.presentation.ui.extension.onNoRippleClick
 import com.alarmy.near.presentation.ui.theme.NearTheme
@@ -26,8 +30,10 @@ fun TermsAgreementItem(
     Row(
         modifier =
             Modifier
-                .padding(vertical = 14.dp),
+                .padding(vertical = 14.dp)
+                .onNoRippleClick(onclick),
         verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
     ) {
         // 체크박스 클릭 시 체크 상태만 변경
         NearCheckbox(
@@ -39,18 +45,35 @@ fun TermsAgreementItem(
 
         // 텍스트 영역 클릭 시 웹뷰로 이동
         Text(
-            modifier =
-                Modifier
-                    .fillMaxWidth()
-                    .onNoRippleClick(onclick),
             text = text,
             style = NearTheme.typography.B2_14_MEDIUM,
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Image(
+            painter = painterResource(R.drawable.ic_front_24_gray),
+            contentDescription = null,
         )
     }
 
     if (showDivider) {
         HorizontalDivider(
             color = NearTheme.colors.GRAY03_EBEBEB,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TermsAgreementItemPreview() {
+    NearTheme {
+        TermsAgreementItem(
+            text = "개인정보동의~",
+            isChecked = true,
+            onCheckedChange = {},
+            onclick = {},
+            showDivider = true,
         )
     }
 }
