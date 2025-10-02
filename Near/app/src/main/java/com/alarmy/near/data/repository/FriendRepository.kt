fd435e0c2f8e10a7fdafdc5ef17b969268cb5259
stopstone <wltjr6432@naver.com>
2025-10-02 19:12:54 +0900

    feat: 연락처 기반 친구 추가 API 연동
    - 연락처에 있는 친구들을 서버에 일괄 등록하기 위한 `initFriends` API를 추가하고 관련 로직을 구현했습니다.
    - 친구 추가 시 연락 주기가 설정된 연락처만 필터링하여 서버에 요청합니다.

--- Near/app/src/main/java/com/alarmy/near/data/repository/FriendRepository.kt
+++ Near/app/src/main/java/com/alarmy/near/data/repository/FriendRepository.kt
@@ -2,8 +2,10 @@ package com.alarmy.near.data.repository
 
 import com.alarmy.near.model.Friend
 import com.alarmy.near.model.FriendRecord
+import com.alarmy.near.network.response.FriendInitItemEntity
 import com.alarmy.near.model.friendsummary.FriendSummary
 import com.alarmy.near.model.monthly.MonthlyFriend
+import com.alarmy.near.presentation.feature.friendcontactcycle.model.FriendContactUIModel
 import kotlinx.coroutines.flow.Flow
 
 interface FriendRepository {
@@ -23,4 +25,6 @@ interface FriendRepository {
     fun fetchFriendRecord(friendId: String): Flow<List<FriendRecord>>
 
     fun recordContact(friendId: String): Flow<String>
+
+    fun initFriends(contacts: List<FriendContactUIModel>, providerType: String): Flow<List<FriendInitItemEntity>>
 }
