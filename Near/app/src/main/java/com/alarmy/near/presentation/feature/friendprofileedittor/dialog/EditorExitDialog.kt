d4f26450322234cc6ece672b93242941d4407b08
StopStone <wltjr6432@naver.com>
2025-10-06 20:11:21 +0900

    refactor: 프로필 수정 화면 이탈 다이얼로그에 공용 컴포넌트 적용
    - `EditorExitDialog`에서 직접 `AlertDialog`를 구현하던 방식에서 공용 다이얼로그 컴포저블인 `NearBasicDialog`를 사용하도록 변경했습니다.

--- Near/app/src/main/java/com/alarmy/near/presentation/feature/friendprofileedittor/dialog/EditorExitDialog.kt
+++ Near/app/src/main/java/com/alarmy/near/presentation/feature/friendprofileedittor/dialog/EditorExitDialog.kt
@@ -1,15 +1,11 @@
 package com.alarmy.near.presentation.feature.friendprofileedittor.dialog
 
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.alarmy.near.R
+import com.alarmy.near.presentation.ui.component.dialog.NearBasicDialog
 import com.alarmy.near.presentation.ui.theme.NearTheme
 
 @Composable
@@ -18,33 +14,14 @@ internal fun EditorExitDialog(
     onDismissRequest: () -> Unit,
     onConfirm: () -> Unit,
 ) {
-    AlertDialog(
-        modifier = modifier,
-        onDismissRequest = onDismissRequest,
-        title = {
-            Text(text = stringResource(R.string.editor_exit_title))
-        },
-        text = {
-            Text(
-                text =
-                    stringResource(R.string.editor_exit_content),
-            )
-        },
-        confirmButton = {
-            TextButton(
-                onClick = onConfirm,
-            ) {
-                Text(stringResource(R.string.editor_exit_confirm))
-            }
-        },
-        dismissButton = {
-            TextButton(
-                onClick = onDismissRequest,
-            ) {
-                Text(stringResource(R.string.editor_exit_dismiss))
-            }
-        },
-        shape = RoundedCornerShape(24.dp),
+    NearBasicDialog(
+        onDismiss = onDismissRequest,
+        title = stringResource(R.string.editor_exit_title),
+        body = stringResource(R.string.editor_exit_content),
+        dismissButtonText = stringResource(R.string.editor_exit_dismiss),
+        confirmButtonText = stringResource(R.string.editor_exit_confirm),
+        onDismissButtonClick = onDismissRequest,
+        onConfirmButtonClick = onConfirm,
     )
 }
 
