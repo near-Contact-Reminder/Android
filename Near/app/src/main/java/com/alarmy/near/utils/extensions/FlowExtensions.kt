fbb8f539abb84e312bc04d90ba12a9c2d3cee6e0
stopstone <wltjr6432@naver.com>
2025-10-15 17:18:36 +0900

    refactor: 에러 메시지 스트링 추가 및 예외 처리 로직 구현
    - 네트워크, 인증, 서버 등 다양한 상황에 대한 에러 메시지를 `strings.xml`에 추가했습니다.
    - `apiCallFlow`에서 발생하는 예외를 `NearException`으로 변환하여 처리하도록 `FlowExtensions`를 수정했습니다.

--- Near/app/src/main/java/com/alarmy/near/utils/extensions/FlowExtensions.kt
+++ Near/app/src/main/java/com/alarmy/near/utils/extensions/FlowExtensions.kt
@@ -1,9 +1,16 @@
 package com.alarmy.near.utils.extensions
 
+import com.alarmy.near.core.exception.ExceptionMapper
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
 inline fun <T> apiCallFlow(crossinline apiCall: suspend () -> T): Flow<T> =
     flow {
-        emit(apiCall())
+        try {
+            emit(apiCall())
+        } catch (throwable: Throwable) {
+            // NearException으로 변환 후 재throw
+            val nearException = ExceptionMapper.mapToAppException(throwable)
+            throw nearException
+        }
     }
