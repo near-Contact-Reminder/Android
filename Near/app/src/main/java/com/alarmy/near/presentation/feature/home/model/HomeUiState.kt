01267f0513bb55f9aa3aed902dad7b32f3cf17ac
kwakjoohyeong <rhkrwngud445@gmail.com>
2025-11-06 01:42:09 +0900

    refactor: shimmer 적용 및 home uiState로 상태 단일 관리

--- Near/app/src/main/java/com/alarmy/near/presentation/feature/home/model/HomeUiState.kt
+++ Near/app/src/main/java/com/alarmy/near/presentation/feature/home/model/HomeUiState.kt
@@ -1,9 +1,27 @@
 package com.alarmy.near.presentation.feature.home.model
 
-sealed interface HomeUiState {
-    data object Loading : HomeUiState
+import com.alarmy.near.model.friendsummary.FriendSummary
+import com.alarmy.near.model.member.MemberInfo
+import com.alarmy.near.model.monthly.MonthlyFriend
 
+data class HomeUiState(
+    val memberInfo: MemberInfo? = null,
+    val monthlyFriendUIState: MonthlyFriendUIState = MonthlyFriendUIState.Loading,
+    val myFriendUIState: MyFriendUIState = MyFriendUIState.Loading,
+)
+
+sealed interface MonthlyFriendUIState {
     data class Success(
-        val data: Any,
-    ) : HomeUiState
+        val monthlyFriends: List<MonthlyFriend>,
+    ) : MonthlyFriendUIState
+
+    data object Loading : MonthlyFriendUIState
+}
+
+sealed interface MyFriendUIState {
+    data class Success(
+        val myFriends: List<FriendSummary>,
+    ) : MyFriendUIState
+
+    data object Loading : MyFriendUIState
 }
