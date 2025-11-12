72ac684437d174bce8ae310319f64cfa14f41445
stopstone <wltjr6432@naver.com>
2025-11-13 00:58:44 +0900

    feat: `NearBottomSheet` 미리보기 추가
    `@Preview` 어노테이션을 사용하여 `NearBottomSheet` 컴포저블의 미리보기를 추가했습니다.

--- Near/app/src/main/java/com/alarmy/near/presentation/ui/component/bottomsheet/NearBottomSheet.kt
+++ Near/app/src/main/java/com/alarmy/near/presentation/ui/component/bottomsheet/NearBottomSheet.kt
@@ -1,14 +1,22 @@
 package com.alarmy.near.presentation.ui.component.bottomsheet
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.alarmy.near.presentation.ui.theme.NearTheme
 
@@ -30,11 +38,22 @@ fun NearBottomSheet(
             onDismissRequest = onDismiss,
             sheetState = bottomSheetState,
             dragHandle = {
-                BottomSheetDefaults.DragHandle(
-                    color = NearTheme.colors.BLACK_1A1A1A.copy(alpha = 0.1f),
-                    width = 36.dp,
-                    height = 5.dp,
-                )
+                Box(
+                    modifier =
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(top = 12.dp, bottom = 24.dp),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Box(
+                        modifier =
+                            Modifier
+                                .width(36.dp)
+                                .height(5.dp)
+                                .clip(RoundedCornerShape(100.dp))
+                                .background(NearTheme.colors.BLACK_1A1A1A.copy(alpha = 0.1f)),
+                    )
+                }
             },
             modifier = modifier,
             containerColor = NearTheme.colors.WHITE_FFFFFF,
@@ -50,3 +69,16 @@ fun NearBottomSheet(
         }
     }
 }
+
+@Preview
+@Composable
+fun NearBottomSheetPreview() {
+    NearTheme {
+        NearBottomSheet(
+            isVisible = true,
+            onDismiss = {  },
+            content = {},
+        )
+    }
+}
+
